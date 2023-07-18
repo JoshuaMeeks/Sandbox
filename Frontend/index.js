eaa7e4cb19b1e9f393929148f13b43b89daac137
Joshua Meeks <joshuameeks@Joshuas-MBP.attlocal.net>
2023-07-17 23:26:02 -0400

    Loosely following Dave Gray's YouTube video of NodeJS for Beginners. Change path from hardcoded to dirname

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,15 +1,37 @@
-const button = document.querySelector('button');
+const setButton = document.querySelector('.set-button');
+const retrieveButton = document.querySelector('.retrieve-button');
 const input = document.querySelector('input');
 
-async function userData() {
-    const res = await fetch('http://localhost:8080/api');
-    const data = await res.json();
-    console.log(data);
-}
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
+const retrieveData = () => {
     fetch('http://localhost:8080/api')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showData(data.data))
+}
+
+const showData = (data) => {
+    const dataContainer = document.querySelector('.data-container');
+    const dataText = document.createElement('h1');
+    dataText.innerText = data;
+    dataContainer.appendChild(dataText);
+}
+
+const setData = (data) => {
+    fetch('http://localhost:8080/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data })
+    })
+}
+
+setButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    user = input.value
+    setData(user); 
+    input.value = '';
+})
+
+retrieveButton.addEventListener('click', () => {
+    retrieveData();
 })
